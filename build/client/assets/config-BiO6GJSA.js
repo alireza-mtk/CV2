5783cc7bd199835d591990f154684de3a3126b50
alireza-mtk <alireza.motekalemi.ronasteb@gmail.com>
2024-05-25 17:38:15 +0330

    git ignore cm

---
+++ build/client/assets/config-BiO6GJSA.js
@@ -0,0 +1,5 @@
+const i="Alireza Motekalemi",t="Designer",e=["Developer","Creative mind","Digital Marketer"],s="https://hamishw.com",o="@hamishMW",n="@Hamish",a="alireza-mtk",c="https://github.com/HamishMW/portfolio",r=`__  __  __
+\\ \\ \\ \\ \\∕
+ \\ \\∕\\ \\
+  \\∕  \\∕
+`,m={name:i,role:t,disciplines:e,url:s,twitter:o,figma:n,github:a,repo:c,ascii:r};export{m as c};
---
+++ build/client/assets/slice-annotation-large-tqgCvlH_.js
@@ -0,0 +1,1 @@
+const n="/assets/slice-annotation-large-BMgELH_L.png";export{n as s};
